67e5c72202c9e8396dfc6140c4a89f85cdbc9b52
chrisahart <chrissahart@gmail.com>
2023-08-07 18:33:18 +0100

    GUI working

--- extension/content.js
+++ extension/content.js
@@ -26,12 +26,12 @@ function extractRating(htmlSource) {
 
 function extractNameFromHtmlSource() {
     // Works on Majestic wine search page only, returns productInfo
-  console.log("extractNameFromHtmlSource")
   const productElements = document.querySelectorAll('a.product-name.t-not-link');
   const majestic_names = [];
   const majestic_id = [];
   const majestic_price = [];
 
+  //  Get all values
   productElements.forEach((element) => {
     const dataEnhancedProductClick = element.getAttribute('data-enhanced-productclick');
     if (dataEnhancedProductClick) {
@@ -42,21 +42,39 @@ function extractNameFromHtmlSource() {
         majestic_price.push(productInfo.price);
       }
     }
-    
   });
 
-  return [majestic_names, majestic_id, majestic_price];
+  //  Sort by majestic_price ascending
+  const sortedIndex = majestic_price.map((_, index) => index).sort((a, b) => majestic_price[a] - majestic_price[b]);
+  const sortedNames = sortedIndex.map((index) => majestic_names[index]);
+  const sortedIds = sortedIndex.map((index) => majestic_id[index]);
+  const sortedPrices = sortedIndex.map((index) => majestic_price[index]);
+
+  return [sortedNames, sortedIds, sortedPrices];
 }
 
-  
-  console.log("Start of content script")
+ function displayProductNames() {
   const [majestic_names, majestic_id, majestic_price] = extractNameFromHtmlSource();
 
-    console.log("Found " + majestic_names.length + " wines")
+  if (majestic_names.length > 0) {
+    const productNamesElement = document.createElement("div");
+    productNamesElement.style.position = "fixed";
+    productNamesElement.style.bottom = "20px";
+    productNamesElement.style.right = "20px";
+    productNamesElement.style.background = "#fff";
+    productNamesElement.style.padding = "5px";
+    productNamesElement.style.border = "1px solid #ccc";
+    productNamesElement.style.maxWidth = "1000px";
+
+    productNamesElement.textContent = `Detected ${majestic_names.length} wines:`;
 
     majestic_names.forEach((name, index) => {
 
       console.log('Processing wine number ' + index)
+      console.log('Price ' + majestic_price[index]) 
+
+
+      const productNameElement = document.createElement("div");
 
   const productName = name.replace(/[^\w\s]/g, '');
   const URL = `https://www.vivino.com/search/wines?q=${encodeURIComponent(productName)}`;
@@ -71,6 +89,27 @@ function extractNameFromHtmlSource() {
       // Data processing log
       console.log(majestic_id[index] + ", " + productName + ", " + 
                   majestic_price[index] + ", " + Region + ", " + Rating)
+         
+        const vivinoSearchLink = document.createElement("a");
+        vivinoSearchLink.textContent = productName;
+        vivinoSearchLink.href = ID;
+        vivinoSearchLink.target = "_blank";
+  
+        const ratingElement = document.createElement("span");
+        ratingElement.textContent = `, Rating: ${Rating}, Price: ${ majestic_price[index]}`;
+  
+        productNameElement.appendChild(vivinoSearchLink);
+        productNameElement.appendChild(ratingElement);
+        productNamesElement.appendChild(productNameElement);
+
+      })
     })
   })
-});
+  document.body.appendChild(productNamesElement);
+}
+}
+
+  console.log("Start of content script")
+  displayProductNames();
+
+    
